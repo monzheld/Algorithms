b56836b9e2074d484b3046029d36c7c6bdd4d07f
monzheld <monzheld@gmail.com>
2023-01-09 00:02:27 +0900

    refactor: modify make_read_me()

--- utils/count_solved_problems.py
+++ utils/count_solved_problems.py
@@ -70,12 +70,12 @@ def make_read_me(count_info):
     """   
 
     return f"""# Algorithms
-    파이썬 알고리즘 문제 풀이
-    ### Since 2022.12.13 ~
-    {count_info}
-    [알고리즘 문제 풀이 블로그](https://monzheld.tistory.com/category/%E2%8C%A8%EF%B8%8F%20Algorithms)
-    #### 모든 문제는 Python3 로 해결했습니다.
-    """
+파이썬 알고리즘 문제 풀이
+### Since 2022.12.13 ~
+{count_info}
+[알고리즘 문제 풀이 블로그](https://monzheld.tistory.com/category/%E2%8C%A8%EF%B8%8F%20Algorithms)
+#### 모든 문제는 Python3 로 해결했습니다.
+"""
 
 
 def update_readme_md():
