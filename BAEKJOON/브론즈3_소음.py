c0b1b7872b0e9922d25f7b1962757555abf91296
monzheld <monzheld@gmail.com>
2023-06-20 21:59:43 +0900

    recommit

--- BAEKJOON/브론즈3_소음.py
+++
@@ -1,8 +0,0 @@
-question = []
-for _ in range(3):
-    question.append(input())
-
-if question[1] == '+':
-    print(int(question[0]) + int(question[2]))
-else:
-    print(int(question[0]) * int(question[2]))
--- BAEKJOON/브론즈4_입실관리.py
+++
@@ -1,4 +0,0 @@
-n = int(input())
-for _ in range(n):
-    si = input()
-    print(si.lower())
